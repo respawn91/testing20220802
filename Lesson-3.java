c46b2b8d389942c3fc09d9640910b73af8383403
Anton Tarasov <respawn91@gmail.com>
2022-08-09 21:02:49 +0300

    Add files via upload

---
+++ Lesson-3.java
@@ -0,0 +1,247 @@
+// Объявление переменной
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int x;
+      x = 10;
+      
+      System.out.println(x);
+      
+      x = 15;
+      System.out.println(x);
+
+      x = -25;
+      System.out.println(x);
+  }
+}
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int x = 100;
+      System.out.println(x);
+  }
+}
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      double y = 100.12352;
+      
+      System.out.println(y);
+  }
+}
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      String s = "Hello World!";
+      
+      System.out.println(s);
+  }
+}
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      boolean s = false;
+      
+      System.out.println(s);
+  }
+}
+
+// Статическая типизация
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 50;
+      
+      a = "Some text";
+  }
+}
+
+// Динамическая типизация (на PHP)
+<?php
+  
+  $a = 50;
+	echo $a, "\n";
+	
+	$a = -124123.121;
+	echo $a, "\n";
+	
+	$a = "Hello World";
+	echo $a, "\n";
+	
+	$a = false;
+	echo var_export($a), "\n";
+?>
+
+// Арифметика
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 50;
+      int b = 8;
+      
+      int result1 = a + b;
+      System.out.println(result1);
+      
+      int result2 = a * b;
+      System.out.println(result2);
+      
+      int result3 = a - b;
+      System.out.println(result3);
+      
+      // Целочисленное деление
+      int result4 = a / b;
+      System.out.println(result4);
+      
+      // Получение остатка
+      int result5 = a % b;
+      System.out.println(result5);
+  }
+}
+
+// Арифметика
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      double a = 20;
+      double b = 3;
+      
+      double result1 = a + b;
+      System.out.println(result1);
+      
+      double result2 = a * b;
+      System.out.println(result2);
+      
+      double result3 = a - b;
+      System.out.println(result3);
+      
+      // Целочисленное деление
+      double result4 = a / b;
+      System.out.println(result4);
+      
+      // Получение остатка
+      double result5 = a % b;
+      System.out.println(result5);
+  }
+}
+
+
+// Приведение типов
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      byte a = 100;
+      int b = 5;
+      
+      System.out.println(a);
+      System.out.println(b);
+      
+      int result = a + b;
+      System.out.println(result);
+  }
+}
+
+// Деление на 0 (целое)
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 50;
+      int b = 0;
+      
+      int result = a / b;
+      
+      System.out.println(result);
+  }
+}
+
+// Деление на 0 (вещественное)
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      double a = 50;
+      double b = 0;
+      
+      double result = a / b;
+      
+      System.out.println(result);
+  }
+}
+
+// Инкремент и декремент
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 10;
+      System.out.println(a);
+      
+      a = a + 1;
+      System.out.println(a);
+      
+      a++;
+      System.out.println(a);
+      
+      a--;
+      System.out.println(a);
+  }
+}
+
+// Инкремент и декремент
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 10;
+      System.out.println(a);
+      
+      a = a + 1;
+      System.out.println(a);
+      
+      a++;
+      System.out.println(a);
+      
+      a--;
+      System.out.println(a);
+  }
+}
+
+// Приоритет операций
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int result = 3 * (10 / 5 - 3 * (5 - 2));
+      
+      System.out.println(result);
+  }
+}
+
